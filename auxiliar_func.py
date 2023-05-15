95a611dc5412182365dcd94717e1f93bd2fc9d1c
JoelSoleCasale <101803264+JoelSoleCasale@users.noreply.github.com>
2023-05-15 13:18:22 +0200

    aux

--- auxiliar_func.py
+++ auxiliar_func.py
@@ -5,8 +5,8 @@ import numpy as np
 def preprocessing(data: pd.DataFrame, imputation: str = "mean") -> pd.DataFrame:
     df = data.copy()
     df = df.drop('unknown', axis=1)  # Drop unknown column
-    df = df.drop_duplicates() # Drop duplicates
-    
+    df = df.drop_duplicates()  # Drop duplicates
+
     ########## MISSING VALUES ##########
     # Get columns with missing values
     cols_with_missing = df.columns[df.isnull().any()]
@@ -14,6 +14,7 @@ def preprocessing(data: pd.DataFrame, imputation: str = "mean") -> pd.DataFrame:
     df = df.drop(
         cols_with_missing[df[cols_with_missing].isnull().mean() > 0.4], axis=1)
 
+    cols_with_missing = df.columns[df.isnull().any()]
     if imputation == 'mode':
         for col in cols_with_missing:
             df[col] = df[col].fillna(df[col].value_counts().index[0])
@@ -24,7 +25,7 @@ def preprocessing(data: pd.DataFrame, imputation: str = "mean") -> pd.DataFrame:
     for col in ['det_ind_code', 'det_occ_code', 'own_or_self', 'vet_benefits', 'year']:
         df[col] = df[col].astype('category')  # Change to categorical type
 
-    age_bins = [0, 18, 25, 35, 45, 55, 65, 75, 85, 95, 105]
+    age_bins = [-1, 18, 25, 35, 45, 55, 65, 75, 85, 95, 105]
     # Change to categorical type
     df['age'] = pd.cut(df['age'], bins=age_bins, labels=age_bins[:-1])
 
