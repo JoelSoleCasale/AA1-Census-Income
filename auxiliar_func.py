1c5610d9d25d1b97d356167cc9b62e03af665584
JoelSoleCasale <101803264+JoelSoleCasale@users.noreply.github.com>
2023-05-18 09:46:31 +0200

    results1

--- auxiliar_func.py
+++ auxiliar_func.py
@@ -6,19 +6,15 @@ from sklearn.cluster import KMeans
 def downsampling(
     data: pd.DataFrame,
     target: str = 'income_50k',
-    target_freq: float = 0.7,
-    type: str = "random"
+    target_freq: float = 0.7
 ) -> pd.DataFrame:
     """Downsampling of the majority class in a dataset. The downsampling is done in clusters or randomly.
     data: pd.DataFrame
         Dataset to downsample
     target: str
         Name of the target column to balance (default: 'income_50k')
-        Must be a binary column
     target_freq: float
         Frequency of apparition of the majority class to downsample
-    type: str
-        Type of downsampling. It can be "clusters" or "random"
     """
 
     df = data.copy()
@@ -27,44 +23,11 @@ def downsampling(
     df_majority = df[df[target] == majority_class]
     df_minority = df[df[target] != majority_class]
 
-    if type == "clusters":
-        # Aplicar el algoritmo de clustering (K-Means)
-        kmeans = KMeans(n_clusters=10)  # Ajusta el número de clusters deseado
-        cluster_labels = kmeans.fit_predict(df_majority)
+    if target_freq > df_majority.shape[0]/df.shape[0]:
+        return df
 
-        # Obtener el tamaño de cada cluster
-        cluster_sizes = pd.Series(cluster_labels).value_counts().sort_index()
-
-        # Calcular el tamaño objetivo para cada cluster
-        target_sizes = (cluster_sizes * target_freq).astype(int)
-
-        # Reducir o replicar las muestras en cada cluster según el tamaño objetivo
-        downsampled_samples = []
-        for cluster_idx in range(len(cluster_sizes)):
-            cluster_samples = df_majority[cluster_labels == cluster_idx]
-
-            if len(cluster_samples) > target_sizes[cluster_idx]:
-                # Downsampling: seleccionar aleatoriamente las muestras para alcanzar el tamaño objetivo
-                downsampled_samples.append(cluster_samples.sample(
-                    n=target_sizes[cluster_idx], random_state=42))
-            else:
-                # Upsampling: replicar las muestras existentes para alcanzar el tamaño objetivo
-                num_replicas = target_sizes[cluster_idx] - len(cluster_samples)
-                replicas = cluster_samples.sample(
-                    n=num_replicas, replace=True, random_state=42)
-                downsampled_samples.append(
-                    pd.concat([cluster_samples, replicas]))
-
-        # Concatenar las muestras downsampling de todos los clusters
-        downsampled_data = pd.concat(downsampled_samples)
-        df_downsampled = pd.concat([downsampled_data, df_minority])
-
-    elif type == "random":
-        df_majority_downsampled = df_majority.sample(
-            n=int(target_freq/(1-target_freq) * df_minority.shape[0]), random_state=42)
-        df_downsampled = pd.concat([df_majority_downsampled, df_minority])
-
-    # Combine minority class with downsampled majority class
+    df_majority_downsampled = df_majority.sample(
+        n=int(target_freq/(1-target_freq) * df_minority.shape[0]), random_state=42)
     df_downsampled = pd.concat([df_majority_downsampled, df_minority])
 
     return df_downsampled
@@ -74,7 +37,9 @@ def preprocessing(
     data: pd.DataFrame,
     imputation: str = "mode",
     remove_duplicates: bool = True,
-    scaling: str = None
+    scaling: str = None,
+    cat_age: bool = True,
+    generate_dummies: bool = True
 ) -> pd.DataFrame:
     """Preprocessing of the dataset. It removes the unknown values, the columns with more than 40% of missing values,
     imputes the missing values with the mode or the KNN algorithm, converts the categorical variables to numerical
@@ -96,9 +61,9 @@ def preprocessing(
     ########## CATEGORICAL CONVERSION ##########
     for col in ['det_ind_code', 'det_occ_code', 'own_or_self', 'vet_benefits', 'year']:
         df[col] = df[col].astype('category')  # Change to categorical type
-    age_bins = [-1, 18, 25, 35, 45, 55, 65, 75, 85, 95, 105]
-    # Change to categorical type
-    df['age'] = pd.cut(df['age'], bins=age_bins, labels=age_bins[:-1])
+    if cat_age:
+        age_bins = [-1, 18, 25, 35, 45, 55, 65, 75, 85, 95, 105]
+        df['age'] = pd.cut(df['age'], bins=age_bins, labels=age_bins[:-1])
 
     for col in df.select_dtypes(include=['object']).columns:
         df[col] = df[col].astype('category')
@@ -135,5 +100,8 @@ def preprocessing(
     ############################
 
     df['income_50k'] = np.where(df['income_50k'] == ' - 50000.', 0, 1)
+
+    if generate_dummies:
+        df = pd.get_dummies(df)
 
     return df
