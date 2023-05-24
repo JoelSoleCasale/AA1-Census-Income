0b018ec843fbb94065d3bbdde3d01ef33e382104
JoelSoleCasale <101803264+JoelSoleCasale@users.noreply.github.com>
2023-05-24 23:32:35 +0200

    aux func estable

--- auxiliar_func.py
+++ auxiliar_func.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import itertools
+import time
 from typing import Literal
 
 from sklearn.cluster import KMeans
@@ -16,10 +17,12 @@ def expand_dicts(df: pd.DataFrame) -> pd.DataFrame:
     for col in df.columns:
         if df[col].dtype == 'object':
             try:
-                df = pd.concat([df.drop(col, axis=1), df[col].apply(pd.Series)], axis=1)
+                df = pd.concat(
+                    [df.drop(col, axis=1), df[col].apply(pd.Series)], axis=1)
             except:
                 pass
     return df
+
 
 def downsampling(
     data: pd.DataFrame,
@@ -83,7 +86,8 @@ def preprocessing(
         df[col] = df[col].astype('category')  # Change to categorical type
     if cat_age:
         age_bins = [-1, 18, 25, 35, 45, 55, 65, 75, 85, 95, 105]
-        df['age'] = pd.cut(df['age'], bins=age_bins, labels=age_bins[:-1])
+        df['age'] = pd.cut(df['age'], bins=age_bins,
+                           labels=age_bins[:-1]).astype('category')
 
     for col in df.select_dtypes(include=['object']).columns:
         df[col] = df[col].astype('category')
@@ -137,8 +141,6 @@ def cross_validation(
     par_te: dict | None = None,
     target: str = 'income_50k',
     cv: int = 4,
-    scoring: list = ['accuracy', 'f1_macro',
-                     'precision_macro', 'recall_macro'],
     mean_score: bool = True
 ) -> dict:
     """Cross validation of a model. It returns the mean of the metrics of the cross validation.
@@ -152,9 +154,8 @@ def cross_validation(
         Parameters to preprocess the test dataset
     cv: int
         Number of folds for the cross validation
-    scoring: list
-        Metrics to calculate in the cross validation
     """
+    t1 = time.time()
     model = clone(model)  # to reset the model
     if par_te is None:
         par_te = par_tr.copy()
@@ -164,7 +165,7 @@ def cross_validation(
             par_te['imputation'] = 'mode'
 
     scores = {}
-    for score in scoring:
+    for score in ['accuracy', 'f1_macro', 'precision_macro', 'recall_macro']:
         scores[score] = []
 
     kf = KFold(n_splits=cv, shuffle=True, random_state=42)
@@ -185,15 +186,13 @@ def cross_validation(
 
         scores['accuracy'].append(accuracy_score(y_te, y_pred))
         scores['f1_macro'].append(f1_score(y_te, y_pred, average='macro'))
-        scores['precision_macro'].append(
-            precision_score(y_te, y_pred, average='macro'))
-        scores['recall_macro'].append(
-            recall_score(y_te, y_pred, average='macro'))
+        scores['precision_macro'].append(precision_score(y_te, y_pred, average='macro'))
+        scores['recall_macro'].append(recall_score(y_te, y_pred, average='macro'))
 
     if mean_score:
         for score in scores.keys():
             scores[score] = np.mean(scores[score])
-
+    scores['tex'] = time.time() - t1
     return scores
 
 
@@ -201,33 +200,29 @@ def test_preprocess_params(
     model: object,
     prep_grid: dict,
     df: pd.DataFrame,
-    metrics: list = ['accuracy', 'f1_macro',
-                     'precision_macro', 'recall_macro'],
     cv: int = 4,
     verbose: int = 1,
     ignore_errors: bool = False
 ) -> pd.DataFrame:
-    c_names = ['prep_param'] + metrics
+    c_names = ['prep_param','accuracy','f1_macro','precision_macro','recall_macro','tex']
     results = pd.DataFrame(columns=c_names, dtype=object)
 
     if type(prep_grid) != list:
         aux = list(itertools.product(*prep_grid.values()))
         prep_grid = [{k: v for k, v in zip(prep_grid.keys(), combination)} for combination in aux]
-    for par_tr in prep_grid:
-        if verbose > 0:
-            print(f"Adjusting for {list(par_tr.values())}")
+    for i, par_tr in enumerate(prep_grid):
+        if verbose == 0: print(f"it: {i+1}/{len(prep_grid)}", end='\r')
+        if verbose > 0: print(f"it {i}: Adjusting for {list(par_tr.values())}")
         try:
             par_tr['remove_duplicates'] = True
 
-            cross_val_results = cross_validation(model, df, par_tr, cv=cv,
-                                                 scoring=metrics)
-            results = pd.concat([results, pd.DataFrame(
-                {'prep_param': [par_tr]} | cross_val_results)])
+            cross_val_results = cross_validation(model, df, par_tr, cv=cv)
+            results = pd.concat([results, pd.DataFrame({'prep_param': [par_tr]} | cross_val_results)])
         except Exception as e:
             if verbose > 0: print(f"Error in {par_tr}")
             if verbose > 1: print(e)
             if not ignore_errors: raise e
-
+    if verbose == 0: print()
     return results
 
 
@@ -237,33 +232,28 @@ def test_model_params(
     df: pd.DataFrame,
     par_tr: dict,
     par_te: dict | None = None,
-    metrics: list = ['accuracy', 'f1_macro',
-                     'precision_macro', 'recall_macro'],
     cv: int = 4,
-    verbose: int = 1
+    verbose: int = 1,
+    ignore_errors: bool = False
 ) -> pd.DataFrame:
-    c_names = ['model_param'] + metrics
+    c_names = ['model_param','accuracy','f1_macro','precision_macro','recall_macro','tex']
     results = pd.DataFrame(columns=c_names, dtype=object)
 
     if type(mod_grid) != list:
         aux = list(itertools.product(*mod_grid.values()))
         mod_grid = [{k: v for k, v in zip(mod_grid.keys(), combination)} for combination in aux]
-    for par_model in mod_grid:
-        if verbose > 0:
-            print(f"Adjusting for {list(par_model.values())}")
+    for i, par_model in enumerate(mod_grid):
+        if verbose == 0: print(f"it: {i+1}/{len(mod_grid)}", end='\r')
+        if verbose > 0: print(f"Adjusting for {list(par_model.values())}")
         try:
             model.set_params(**par_model)
-
-            cross_val_results = cross_validation(model, df, par_tr, par_te, cv=cv,
-                                                 scoring=metrics)
-            results = pd.concat([results, pd.DataFrame(
-                {'model_param': [par_model]} | cross_val_results)])
+            cross_val_results = cross_validation(model, df, par_tr, par_te, cv=cv)
+            results = pd.concat([results, pd.DataFrame({'model_param': [par_model]} | cross_val_results)])
         except Exception as e:
-            if verbose > 0:
-                print(f"Error in {par_model}")
-            if verbose > 1:
-                print(e)
-
+            if verbose > 0: print(f"Error in {par_model}")
+            if verbose > 1: print(e)
+            if not ignore_errors: raise e
+    if verbose == 0: print()
     return results
 
 # tests the best preprocessing combination with a model
@@ -272,35 +262,41 @@ def test_model_params(
 # keep the best 5 model parameters
 # repeat the last two steps searching over the best 5 preprocessing combinations and 5 model parameters until no improvement is found
 # return a dataframe with all the trained models and their metrics sorted by the target metric
+
+
 def search_best_combination(
     model: object,
     model_params_grid: dict,
     prep_params_grid: dict,
     df: pd.DataFrame,
-    target_metric: Literal['accuracy', 'f1_macro', 'precision_macro', 'recall_macro'] = 'f1_macro',
+    target_metric: Literal['accuracy', 'f1_macro',
+                           'precision_macro', 'recall_macro'] = 'f1_macro',
     cv: int = 4,
     N: int = 5,
     verbose: int = 1,
-    max_iter: int = 10
+    max_iter: int = 10,
+    ignore_errors: bool = True
 ) -> pd.DataFrame:
 
     best_mod_param = [{k: v[0] for k, v in model_params_grid.items()}]
     best_prep_param = []
 
-    results = pd.DataFrame(columns=['prep_param', 'model_param',
-                           'accuracy', 'f1_macro', 'precision_macro', 'recall_macro'],
-                           index=[0,1], dtype=object)
-    
-    
+    results = pd.DataFrame(columns=['prep_param', 'model_param', 'accuracy',
+                            'f1_macro', 'precision_macro', 'recall_macro', 'tex'],
+                           index=[0, 1], dtype=object)
+
     def split_computed(mod_par: list, prep_par: list):
         '''returns a list of all the non computed combinations of preprocessing and model parameters
         and returns a list of the computed model and preprocessing parameters and the results of the
         computed combinations'''
         nonlocal results
-        nc_prep_par, nc_mod_par, computed = [], [], pd.DataFrame(columns=results.columns)
-        if isinstance(mod_par, dict) or isinstance(prep_par, dict): return mod_par, prep_par, computed
+        nc_prep_par, nc_mod_par = [], []
+        computed = pd.DataFrame(columns=results.columns)
+        if isinstance(mod_par, dict) or isinstance(prep_par, dict):
+            return mod_par, prep_par, computed
         for m_par, p_par in zip(mod_par, prep_par):
-            row = results[(results['prep_param'] == p_par) & (results['model_param'] == m_par)]
+            row = results[(results['prep_param'] == p_par) &
+                          (results['model_param'] == m_par)]
             if row.empty:
                 nc_prep_par.append(p_par)
                 nc_mod_par.append(m_par)
@@ -313,25 +309,34 @@ def search_best_combination(
         nonlocal best_prep_param, results
         model.set_params(**mod_param)
         _, prep_par_list, computed = split_computed([mod_param]*len(prep_par_list), prep_par_list)
-        prep_par = test_preprocess_params(model, prep_par_list, df, cv=cv, verbose=verbose-2).sort_values(by=target_metric, ascending=False).reset_index(drop=True)
+        prep_par = test_preprocess_params(
+            model, prep_par_list, df, cv=cv, verbose=verbose-2, ignore_errors=ignore_errors)
+        prep_par = prep_par.sort_values(
+            by=target_metric, ascending=False).reset_index(drop=True)
         prep_par['model_param'] = pd.Series([mod_param]*len(prep_par))
         results = pd.concat([results, prep_par])
-        best_prep_param = pd.concat([prep_par,computed])['prep_param'][:N].tolist()
+        best_prep_param = pd.concat([prep_par, computed])[
+            'prep_param'][:N].tolist()
 
     def update_mod_params(prep_param: dict, mod_par_list: list):
         '''searches the best model parameters for a given preprocessing parameters'''''
         nonlocal best_mod_param, results
         mod_par_list, _, computed = split_computed(mod_par_list, [prep_param]*len(mod_par_list))
-        mod_par = test_model_params(model, mod_par_list, df, prep_param, cv=cv, verbose=verbose-2).sort_values(by=target_metric, ascending=False).reset_index(drop=True)
+        mod_par = test_model_params(model, mod_par_list, df, prep_param,
+                                    cv=cv, verbose=verbose - 2, ignore_errors=ignore_errors)
+        mod_par = mod_par.sort_values(
+            by=target_metric, ascending=False).reset_index(drop=True)
         mod_par['prep_param'] = pd.Series([prep_param]*len(mod_par))
         results = pd.concat([results, mod_par])
-        best_mod_param = pd.concat([mod_par,computed])['model_param'][:N].tolist()
+        best_mod_param = pd.concat([mod_par, computed])[
+            'model_param'][:N].tolist()
 
     best_metric = 0
-    for i in range(1,max_iter+1):
+    for i in range(1, max_iter+1):
         if verbose > 0: print(f'===Iteration {i}===')
         if verbose > 0: print(f'Searching preprocessing parameters...')
         update_prep_params(best_mod_param[0], prep_params_grid)
+        
         if verbose > 0: print(f'Searching model parameters...')
         update_mod_params(best_prep_param[0], model_params_grid)
 
@@ -341,12 +346,14 @@ def search_best_combination(
             best_metric = results[target_metric].max()
         else:
             break
-        
+
         # update the parameter grid without duplicates
         results['temp1'] = results['prep_param'].astype(str)
-        prep_params_grid = results.drop_duplicates(subset='temp1').drop('temp1', axis=1)['prep_param'][:N].tolist()
+        prep_params_grid = results.drop_duplicates(subset='temp1').drop(
+            'temp1', axis=1)['prep_param'][:N].tolist()
         results['temp2'] = results['model_param'].astype(str)
-        model_params_grid = results.drop_duplicates(subset='temp2').drop('temp2', axis=1)['model_param'][:N].tolist()
+        model_params_grid = results.drop_duplicates(subset='temp2').drop(
+            'temp2', axis=1)['model_param'][:N].tolist()
 
         results = results.drop(['temp1', 'temp2'], axis=1)
 
